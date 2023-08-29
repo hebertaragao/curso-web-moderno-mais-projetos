7af015866dd6169d8c4fa7e45fcbf15d6cc62984
hebertaragao <hebert.aragao@outlook.com>
2023-08-29 18:02:11 -0300

    strings.js

---
+++ Fundamentos/strings.js
@@ -0,0 +1,10 @@
+const escola = " Cod3r"
+console.log(escola.charAt(5))
+console.log(escola.charAt(6))
+console.log(escola.charCodeAt(4))
+console.log(escola.indexOf('4'))
+console.log(escola.substring(2))
+console.log(escola.substring(0,4))
+console.log('Escola'.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+console.log('Ana,Maria,Pedro'.split(','))
