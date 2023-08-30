d1085e7d3f88af5f0ba0693aaea4b4b28c291301
hebertaragao <hebert.aragao@outlook.com>
2023-08-30 18:40:54 -0300

    booleanos.js

---
+++ Fundamentos/booleanos.js
@@ -0,0 +1,31 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('Os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar...')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = '' // se a variável estiver vazia impprime desconhecido caso contrário aparecerá o nome escolhido na variável
+console.log(nome || 'Desconhecido')
