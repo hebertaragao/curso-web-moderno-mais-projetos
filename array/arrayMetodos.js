baeba4386c1fb7f738821a69e8f0d3c90f813170
hebertaragao <hebert.aragao@outlook.com>
2023-09-13 23:00:13 -0300

    arrayMetodos.js

---
+++ array/arrayMetodos.js
@@ -0,0 +1,29 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o ultimo elemento do array
+console.log(pilotos)
+
+pilotos.push('Vertappen') // adiciona um novo elemento 
+console.log(pilotos)
+
+pilotos.shift(pilotos) //remove primeiro elemento
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') // adiciona elemento na primeira posição
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1) 
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
+
