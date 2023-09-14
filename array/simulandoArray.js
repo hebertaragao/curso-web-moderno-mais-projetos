b3dd2cbd69e41c69a32e4e8fdd7ce4f6ac80b923
hebertaragao <hebert.aragao@outlook.com>
2023-09-13 23:06:55 -0300

    simulandoArray.js

---
+++ array/simulandoArray.js
@@ -0,0 +1,12 @@
+const quaseArray = {0: 'Rafaela', 1: 'Ana', 2: 'Bia'}
+console.log(quaseArray)
+Object.defineProperty(quaseArray, 'toString', {
+    value: function(){return Object.values(this)},
+    Enumerator: false
+})
+
+console.log(quaseArray[0])
+
+const meuArray = ['Rafaela', 'Ana', 'Bia']
+console.log(quaseArray.toString(), meuArray)
+
