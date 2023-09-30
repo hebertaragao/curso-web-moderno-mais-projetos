3a661cb285e1eeed7baca43e17d5d2c743ecb370
hebertaragao <hebert.aragao@outlook.com>
2023-09-29 21:18:24 -0300

    map3.js

---
+++ array/map3.js
@@ -0,0 +1,22 @@
+Array.prototype.map2 = function(callback){
+
+    const newAraay = []
+    for(let i = 0; i < this.length; i++){
+        newAraay.push(callback(this[i], i, this))
+    }
+    return newAraay
+}
+
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os preços
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco 
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
